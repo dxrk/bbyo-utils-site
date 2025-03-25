c3ed0a47176ea83f0b86855144f9ecdb1260e2c7
Benjamin Spencer Talesnik <bentalesnik@gmail.com>
2025-03-25 12:46:07 -0400

    Update font loading to use WOFF format for Graphik fonts

--- lib/font-utils.ts
+++ lib/font-utils.ts
@@ -5,36 +5,36 @@ export function getFontFaceStyle(): string {
   // Read fonts directly from filesystem
   const fontsDir = path.join(process.cwd(), "lib", "fonts");
   const regularFont = fs.readFileSync(
-    path.join(fontsDir, "Graphik-Regular.woff2"),
+    path.join(fontsDir, "Graphik-Regular.woff"),
     "base64"
   );
   const lightFont = fs.readFileSync(
-    path.join(fontsDir, "Graphik-Light.woff2"),
+    path.join(fontsDir, "Graphik-Light.woff"),
     "base64"
   );
   const mediumFont = fs.readFileSync(
-    path.join(fontsDir, "Graphik-Medium.woff2"),
+    path.join(fontsDir, "Graphik-Medium.woff"),
     "base64"
   );
 
   return `
     @font-face {
       font-family: 'Graphik';
-      src: url(data:font/woff2;base64,${regularFont}) format('woff2');
+      src: url(data:font/woff;base64,${regularFont}) format('woff');
       font-weight: 400;
       font-style: normal;
       font-display: swap;
     }
     @font-face {
       font-family: 'Graphik';
-      src: url(data:font/woff2;base64,${lightFont}) format('woff2');
+      src: url(data:font/woff;base64,${lightFont}) format('woff');
       font-weight: 300;
       font-style: normal;
       font-display: swap;
     }
     @font-face {
       font-family: 'Graphik';
-      src: url(data:font/woff2;base64,${mediumFont}) format('woff2');
+      src: url(data:font/woff;base64,${mediumFont}) format('woff');
       font-weight: 500;
       font-style: normal;
       font-display: swap;
